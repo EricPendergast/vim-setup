fcff466c15919a7353669980691abf0f103f4cbb
Eric Pendergast <18491983+EricPendergast@users.noreply.github.com>
2024-06-19 11:42:00 -0700

    Move code around
    Moved lua code out of vimrc
    Moved coc setup out of vimrc

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,3 +9,36 @@ vim.env.NVIM_TUI_ENABLE_TRUE_COLOR = 1
 
 -- Source the .vimrc file
 vim.cmd("source ~/.vim/vimrc")
+
+
+require("nvim-tree").setup({
+    hijack_netrw = false,
+    hijack_directories = {
+        enable = false
+    },
+    renderer = {
+        icons = {
+            show = {
+                git = true,
+                file = false,
+                folder = false,
+                folder_arrow = true,
+            },
+            glyphs = {
+                folder = {
+                    arrow_closed = "▸",
+                    arrow_open = "▾",
+                },
+                git = {
+                    unstaged = "✗",
+                    staged = "✓",
+                    unmerged = "⌥",
+                    renamed = "➜",
+                    untracked = "★",
+                    deleted = "⊖",
+                    ignored = "◌",
+                },
+            },
+        },
+    },
+})
